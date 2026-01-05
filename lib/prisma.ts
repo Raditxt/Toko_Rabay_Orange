febe6a1410896669f666e9c80a815d47b426838f
Raditxt <radityamulyaakbar2@gmail.com>
2026-01-05 12:56:30 +0800

    fix api products and prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,19 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@/app/generated/prisma/client"; // path sesuai output generator
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = global as unknown as {
+const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["error"],
+    adapter,
+    log: ["error", "warn"],
   });
 
 if (process.env.NODE_ENV !== "production") {
